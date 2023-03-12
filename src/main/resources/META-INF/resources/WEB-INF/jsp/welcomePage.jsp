7d12bb5399042adfde86d7e98d3bcc5328ab14bc
Maha <Maha@Inspiron-Maha3558>
2023-03-12 14:22:22 +0530

    First Commit ....

---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcomePage.jsp
@@ -0,0 +1,7 @@
+<%@ include file="common/header.jspf" %>
+<%@ include file="common/navbar.jspf" %>
+		<div class="container">
+		<h3>Welcome ${name}</h3>
+		<a href="list-todo">Manage</a> your lists here
+		</div>
+<%@ include file="common/footer.jspf" %>
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1001,'manja','Get AWS Certified',CURRENT_DATE(),false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1002,'manja','Get Azure Certified',CURRENT_DATE(),false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1003,'manja','Learn DevOps',CURRENT_DATE(),false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1004,'manja','Learn Java 17',CURRENT_DATE(),false);
